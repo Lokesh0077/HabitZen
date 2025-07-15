bba7f1fe6391bd8771d149ee12b99aeb0ea17a84
Lokesh0077 <143614284+Lokesh0077@users.noreply.github.com>
2025-07-16 00:02:53 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,2 @@
-'use strict';
-
-self.addEventListener('push', function (event) {
-  if (!event.data) {
-    console.log('Push event but no data');
-    return;
-  }
-  const data = event.data.json();
-  const title = data.title || 'HabitZen Reminder';
-  const options = {
-    body: data.body,
-    // You can add icons, badges, etc. here
-    // icon: '/icon.png',
-  };
-  event.waitUntil(self.registration.showNotification(title, options));
-});
-
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  // This opens the app to the root page.
-  event.waitUntil(
-    clients.openWindow('/')
-  );
-});
+// This file is intentionally left blank. 
+// It is used to help Next.js recognize the service worker path.
